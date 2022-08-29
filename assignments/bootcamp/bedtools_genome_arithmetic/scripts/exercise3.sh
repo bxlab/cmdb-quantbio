9f120572d43ace93e5ec98b71e23dc84b6fa5726
Kate Weaver <kweave23@jhu.edu>
2022-08-29 08:34:56 -0400

    path for genes.bed

--- assignments/bootcamp/bedtools_genome_arithmetic/scripts/exercise3.sh
+++ assignments/bootcamp/bedtools_genome_arithmetic/scripts/exercise3.sh
@@ -3,5 +3,5 @@
 #USAGE: bash exercise3.sh input_VCF
 
 awk '/^#/{next} {print $1,$2-1, $2}' $1 > variants.bed
-sort -k1,1 -k2,2n genes.bed > genes.sorted.bed
-bedtools closest -a variants.bed -b genes.sorted.bed | head
+sort -k1,1 -k2,2n ~/data/bed_files/genes.bed > genes.sorted.bed
+bedtools closest -a variants.bed -b genes.sorted.bed
