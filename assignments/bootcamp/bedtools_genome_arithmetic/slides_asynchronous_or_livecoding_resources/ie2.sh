2d0b459ca395419ab3d8657119d621451dbccbee
Kate Weaver <kweave23@jhu.edu>
2022-08-30 13:01:52 -0400

    update scripts

--- assignments/bootcamp/bedtools_genome_arithmetic/slides_asynchronous_or_livecoding_resources/ie2.sh
+++ assignments/bootcamp/bedtools_genome_arithmetic/slides_asynchronous_or_livecoding_resources/ie2.sh
@@ -6,7 +6,11 @@ vcffile=/Users/cmdb/data/vcf_files/random_snippet.vcf
 
 bedtools intersect -a $genefile -b $vcffile > intersect_out_ie2.bed
 
+#Number of SNPs
+wc -l intersect_out_ie2.bed
 
+#number of unique genes
+cut -f 4 intersect_out_ie2.bed | sort | uniq | wc -l
 
 
 
--- assignments/bootcamp/bedtools_genome_arithmetic/slides_asynchronous_or_livecoding_resources/ie3.sh
+++ assignments/bootcamp/bedtools_genome_arithmetic/slides_asynchronous_or_livecoding_resources/ie3.sh
@@ -1,5 +1,15 @@
+#USAGE: ie3.sh input_vcf_file
+#broad goal: What is the most common alternate allele for a reference allele of C
+
+#error free code
+
+grep -v "#" $1 | awk '{if ($4 == "C") {print $5}}' | sort | uniq -c
 
 #USAGE: ie3.sh input_vcf_file nucleotide_of_interest
+#refine the goal to work for any nucleotide
 
+
+#code with error
 nucoi=$2
 grep -v "#" $1 | awk '{if ($4 == $nucoi) {print $5}}' | sort | uniq -c
+
