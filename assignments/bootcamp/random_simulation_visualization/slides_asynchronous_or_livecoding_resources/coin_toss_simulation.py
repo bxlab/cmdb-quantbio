839a72eb657751579cc8aba491b79ad573a60e4c
Kate Weaver <kweave23@jhu.edu>
2022-09-01 12:34:48 -0400

    starting script for students

---
+++ assignments/bootcamp/random_simulation_visualization/slides_asynchronous_or_livecoding_resources/coin_toss_simulation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import numpy
+
+def simulate_coin_toss(n_tosses, prob_heads = 0.5, seed=None):
+    '''
+    Input: n_tosses, an integer, number of coin tosses to simulate
+           prob_heads, a float, the probability the coin toss will return heads; default is 0.5, meaning a fair coin
+           seed, an integer, the random seed that will be used in the simulation
+    Output: results_arr, an array of 1's and 0's with 1's representing heads and 0's representing tails
+    Resources: https://numpy.org/doc/stable/reference/random/generated/numpy.random.choice.html
+    '''
+    if seed is not None:
+        numpy.random.seed(seed)
+    results_arr = numpy.random.choice([0,1], size=n_tosses, p = [1-prob_heads, prob_heads])
+    return (results_arr)
