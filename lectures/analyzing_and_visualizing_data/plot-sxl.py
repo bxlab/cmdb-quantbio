c8d5e927f6a6890c807b55f4f7e54238d5342d79
Frederick Tan <tan@carnegiescience.edu>
2023-09-01 21:28:36 -0400

    Clarify plot and variable names in plot-sxl.py

--- lectures/analyzing_and_visualizing_data/plot-sxl.py
+++ lectures/analyzing_and_visualizing_data/plot-sxl.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 # Get dataset to recreate Fig 3B from Lott et al 2011 PLoS Biology https://pubmed.gov/21346796
 # wget https://github.com/bxlab/cmdb-quantbio/raw/main/assignments/lab/bulk_RNA-seq/extra_data/all_annotated.csv
 
-names = np.loadtxt( "all_annotated.csv", delimiter=",", usecols=0, dtype="<U30", skiprows=1 )
-print( "transcripts: ", names[0:5] )
+transcripts = np.loadtxt( "all_annotated.csv", delimiter=",", usecols=0, dtype="<U30", skiprows=1 )
+print( "transcripts: ", transcripts[0:5] )
 
 samples = np.loadtxt( "all_annotated.csv", delimiter=",", max_rows=1, dtype="<U30" )[2:]
 print( "samples: ", samples[0:5] )
@@ -15,9 +15,9 @@ print( "samples: ", samples[0:5] )
 data = np.loadtxt( "all_annotated.csv", delimiter=",", dtype=np.float32, skiprows=1, usecols=range(2, len(samples) + 2) )
 print( "data: ", data[0:5, 0:5] )
 
-# Find row with name of interest
-for i in range(len(names)):
-    if names[i] == 'FBtr0331261':
+# Find row with transcript of interest
+for i in range(len(transcripts)):
+    if transcripts[i] == 'FBtr0331261':
         row = i
 
 # Find columns with samples of interest
@@ -26,13 +26,17 @@ for i in range(len(samples)):
     if "female" in samples[i]:
         cols.append(i)
 
-# Obtain 
+# Subset data of interest
 expression = data[row, cols]
+
+# Prepare data
+x = samples[cols]
+y = expression
 
 # Plot data
 fig, ax = plt.subplots()
 ax.set_title( "FBtr0331261" )
-ax.plot( expression )
+ax.plot( x, y )
 fig.savefig( "FBtr0331261.png" )
 plt.close( fig )
 
