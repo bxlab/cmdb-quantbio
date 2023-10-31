f842f3743576db835aca60de29b54a7a7db9775c
msauria <mike.sauria@gmail.com>
2023-10-31 12:18:56 -0400

    Updated nanopore livecoding

---
+++ assignments/lab/nanopore-seq/slides_asynchronous_or_livecoding_resources/livecoding.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+
+import matplotlib.pyplot as plt
+
+
+def main():
+    # Load file names from command line
+
+    # Load data from files
+
+    # Find reads that appear more than once in datasets
+
+    # Print statistics about unique vs multimapping reads
+
+    # Parse data into unique and multi-mapping reads for plotting
+
+    # Plot 4 panels, normal and tumor, unique and multi-mapping
+
+# Create a reusable plotting function
+def plot_data(ax, X, Y, label):
+
+# Create a reusable data loading function. Record read name, actual size, and mapped size
+def load_data(fname):
+
+
+main()
